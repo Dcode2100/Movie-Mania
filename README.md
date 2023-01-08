e8893120b3de91529dd202755204cec1a7dc1654
Dikshant Solanki <dcode2100@gmail.com>
2023-01-08 23:00:30 +0530

    api call

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 2. import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; -> this type of import is used when the browserrouter is imported as alice meaning wherever we write router it means browserrouter and the routes and route are not imported using alice.
 
 
-3. 
+3. npm i react responsive carousel is a component for smooth carousel display at required page with autoplay  and autoslide.
+
+4. 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,7 @@
 import React, { useState,useEffect } from 'react'
 import './Home.css';
-
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const Home = () => {
 
@@ -9,11 +10,35 @@ const Home = () => {
      useEffect(() => {    // useEffect is used to fetch data from the API and now we will save this data in the state above
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=b6ae8e682dc29e414a10b8ef7c075acc')
         .then(res => res.json())
-        .then(data => console.log(data.results))
+        .then(data => setPopularMovies(data.results))
     },[])
  
   return (
-    <div>HomePage</div>
+    <>
+      <div className='Poster'>
+        <Carousel
+        showThumbs={false}
+        autoPlay={true} 
+        transitionTime={3}
+        infiniteLoop={true}
+        showStatus={false}
+        >
+          {
+            popularMovies.map(movie =>(
+              <Link style={{textDecoration:'none'}} to={`/movie/${movie.id}`}>
+              <div className='posterImage'>
+                <img src={`https://image.tmdb.org/t/p/original/${movie && movie.poster_path}`}/>
+              </div>
+              <div className='posterImage__overlay'>
+                    <div className='posterImage__title'></div>
+
+              </div>
+              <Link/>
+            ))
+          }
+        </Carousel>
+        </div>
+    </>
   )
 }
 
